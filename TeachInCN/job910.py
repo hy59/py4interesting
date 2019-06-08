0aa8036c26b3bd2e8c74952069ca1e432bb3eea8
Chase Huang <937162905@qq.com>
2019-06-08 10:37:28 +0800

    crawl the data

---
+++ TeachInCN/job910.py
@@ -0,0 +1,75 @@
+'''
+@Author: Chase Huang
+@Date: 2019-06-05 16:43:42
+@LastEditTime: 2019-06-05 19:53:12
+@Description: get the local englisgh teacher job data from job910.com
+'''
+import requests
+import time
+import random
+import pandas as pd
+from lxml import etree
+
+
+class Job910(object):
+    def __init__(self, last_page):
+        self.headers = {
+            "Accept":
+            "text/javascript, application/javascript, application/ecmascript, application/x-ecmascript, */*; q=0.01",
+            "Accept-Encoding":
+            "gzip, deflate",
+            "Connection":
+            "keep-alive",
+            "User-Agent":
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36",
+        }
+        self.start_urls = [
+            'http://www.job910.com/search.aspx?keyword=英语教师&pageSize=20&pageIndex={}'
+            .format(i) for i in range(1, last_page + 1)
+        ]
+
+    def get_data(self):
+        for url in self.start_urls:
+            res = requests.get(url, headers=self.headers)
+            page = url.split('=')[-1]
+            self.parse_data(res, page)
+            print("Get data from page {} successful!".format(page))
+            time.sleep(random.randint(1, 2))
+
+    @staticmethod
+    def parse_data(res, page):
+        if res.status_code == 200:
+            parsed = etree.HTML(res.text)
+
+            title = parsed.xpath('//*[@class="position title"]/a/text()')
+            link = parsed.xpath('//*[@class="position title"]/a/@href')
+            salary = parsed.xpath('//*[@class="salary title"]/text()')
+            company = parsed.xpath('//*[@class="com title adclick"]/text()')
+            area = parsed.xpath('//*[@class="area title2"]/text()')
+            post_time = parsed.xpath('//*[@class="time title2"]/text()')
+            exp_level = parsed.xpath('//*[@class="exp title2"]/text()')
+
+            data = pd.DataFrame({
+                'title': title,
+                'link': link,
+                'salary': salary,
+                'company': company,
+                'area': area,
+                'post_time': post_time,
+                'exp_level': exp_level
+            })
+
+            if page == '1':
+                data.to_csv(
+                    'data/job910.csv', index=False, mode='a', header=True)
+            else:
+                data.to_csv(
+                    'data/job910.csv', index=False, mode='a', header=False)
+        else:
+            print("Can't request {}".format(res.url))
+
+
+if __name__ == "__main__":
+    J = Job910(392)
+    J.get_data()
+    print("All done ...")
---
+++ TeachInCN/jobleadchina.py
@@ -0,0 +1,83 @@
+'''
+@Author: Chase Huang
+@Date: 2019-06-05 15:26:47
+@LastEditTime: 2019-06-05 17:01:35
+@Description: get the data from the jobleadchina website
+'''
+import requests
+import pandas as pd
+from lxml import etree
+
+
+class JobLeadChina(object):
+    def __init__(self, last_page):
+        self.start_urls = [
+            'http://jobleadchina.com/job?job_industry=Teaching&company_name=&page={}'
+            .format(page) for page in range(1, last_page + 1)
+        ]
+
+    def get_data(self):
+        for url in self.start_urls:
+            res = requests.get(url)
+            page = url.split('=')[-1]
+            self.parse_data(res, page)
+            print("Get data from page {} successful!".format(page))
+
+    @staticmethod
+    def parse_data(res, page):
+        if res.status_code == 200:
+            parsed = etree.HTML(res.text)
+
+            title = parsed.xpath('//*[@class="positionTitle"]/a/text()')
+            link = parsed.xpath('//*[@class="positionTitle"]/a/@href')
+            salary = [
+                slr.strip()
+                for slr in parsed.xpath('//*[@class="salaryRange"]/text()')
+            ]
+            company = parsed.xpath('//*[@class="companyName"]/a/text()')
+            com_type = parsed.xpath(
+                '//*[@class="jobThumbnailCompanyIndustry"]/span[1]/text()')
+            area = parsed.xpath(
+                '//*[@class="jobThumbnailCompanyIndustry"]/span[3]/text()')
+            post_time = parsed.xpath('//*[@class="post-time"]/text()')
+            exp_level = parsed.xpath(
+                '//*[@class="jobThumbnailPositionRequire"]/span[3]/text()')
+            edu_level = parsed.xpath(
+                '//*[@class="jobThumbnailPositionRequire"]/span[1]/text()')
+            job_type = parsed.xpath(
+                '//*[@class="jobThumbnailPositionRequire"]/span[5]/text()')
+
+            data = pd.DataFrame({
+                'title': title,
+                'link': link,
+                'salary': salary,
+                'company': company,
+                'com_type': com_type,
+                'area': area,
+                'post_time': post_time,
+                'exp_level': exp_level,
+                'edu_level': edu_level,
+                'job_type': job_type
+            })
+
+            if page == '1':
+                data.to_csv(
+                    'data/jobleadchina.csv',
+                    index=False,
+                    mode='a',
+                    header=True)
+            else:
+                data.to_csv(
+                    'data/jobleadchina.csv',
+                    index=False,
+                    mode='a',
+                    header=False)
+
+        else:
+            print("Can't request {}".format(res.url))
+
+
+if __name__ == "__main__":
+    JLC = JobLeadChina(105)
+    JLC.get_data()
+    print("All done ...")
